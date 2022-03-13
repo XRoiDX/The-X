14d2f823b7deed23e083dbab2c51ade4fde90ecb
X Roid <97869723+XRoiDX@users.noreply.github.com>
2022-03-13 10:01:57 +0530

    Update script.py

--- MikuXProBot/script.py
+++ MikuXProBot/script.py
@@ -11,14 +11,14 @@ MIKU_IMG = (
 TEXXT = "<b>Heya</b> {} <b>Do you Like me?</b>"
 
 MIKU_N_IMG = (
-      "https://telegra.ph/file/837c61d9c51236fea4100.jpg",
-      "https://telegra.ph/file/ee34cf0d7e4782424b777.jpg",
-      "https://telegra.ph/file/5410b02359a2cabc2776b.jpg",
-      "https://telegra.ph/file/b1fc3b2af759999bf3b35.jpg",
-      "https://telegra.ph/file/305b5b3c4527cd439b926.jpg"
+      "",
+      "https://telegra.ph/file/a19aad712f30bca10568d.jpg",
+      "https://telegra.ph/file/72fb184e6d00455618f93.jpg",
+      "https://telegra.ph/file/a19aad712f30bca10568d.jpg",
+      "https://telegra.ph/file/37911c002e2f336419c28.jpg"
 
 )
 
-PM_PHOTO = "https://telegra.ph//file/3a88a958a88e83d85d284.jpg"
+PM_PHOTO = "https://telegra.ph/file/37911c002e2f336419c28.jpg"
 
-MIKU_DISPACHER_PIC = "https://telegra.ph//file/4ce99db6136c77248e0ec.jpg"
+MIKU_DISPACHER_PIC = "https://telegra.ph/file/37911c002e2f336419c28.jpg"
